14f3d2fab345585ba51db5693c6612e8cfb0fc89
Chana023 <jesalchana23@gmail.com>
2025-01-20 15:23:25 +0200

    Added git ignore file

---
+++ Azure/resource_group/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.16.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "terra_random_example" {
+  name     = "example_terra_created"
+  location = "SouthAfricaNorth"
+}
+
+output resource_group_id {
+  value       = azurerm_resource_group.terra_random_example.id
+}
